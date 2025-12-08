c7ad67fc9eefbbc9d47104c6b470d90d3db500cc
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-12-08 22:26:53 +0700

    Set Up Base Project

---
+++ INT305-Final Assign/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div id="header">
+    <h2>Reports</h2>
+    <RouterLink to="/query/1">
+      <div class="QueryExample">
+        1. Product That have Category is mobile
+      </div>
+    </RouterLink>
+    
+    <RouterLink to="/query/2">
+      <div class="QueryExample">
+        2. Cities in Flodia (FL)
+      </div>
+    </RouterLink>
+
+
+
+
+  </div>
+  <div id="detail">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.header {
+  color: white;
+}
+
+a {
+  display: block;
+}
+
+.QueryExample {
+  font-weight: bolder;
+  color: #693CBD
+}
+
+.QueryPractice {
+  font-weight: bolder;
+  font-family: sans-serif;
+  color: #2B8899;
+}
+</style>
